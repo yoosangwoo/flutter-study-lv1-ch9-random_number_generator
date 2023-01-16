2404a5c223a2379c71cc3a416f2b3687d7defd1b
YooSangWoo <qswwsq11@gmail.com>
2023-01-16 12:49:35 +0900

    Initial commit

---
+++ lib/component/number_row.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class NumberRow extends StatelessWidget {
+  final int number;
+  const NumberRow({required this.number, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: number
+          .toInt()
+          .toString()
+          .split('')
+          .map(
+            (e) => Image.asset(
+          'asset/img/$e.png',
+          width: 50,
+          height: 70,
+        ),
+      )
+          .toList(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:random_number_generator/screen/homeScreen.dart';
+
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
+}
